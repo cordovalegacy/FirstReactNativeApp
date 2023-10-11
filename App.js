17ebae4e3d8e20dbedef38e78f4513b950cf9360
Brendan Cordova <cordovalegacy19@gmail.com>
2023-10-10 18:33:11 -0700

    First React Native Push, some basic tags on the App.jsx

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/computer.jpg')}
+        style={styles.computerImage}
+      />
+      <Text style={styles.title}>Explore the World of Computers</Text>
+      <Text style={styles.subtitle}>Unleashing Infinite Possibilities</Text>
+      <Text style={styles.description}>
+        Welcome to our world of innovation and technology. Dive into the
+        fascinating realm of computers, where creativity meets functionality.
+        Discover the latest advancements, learn about programming, and explore
+        the limitless opportunities that the digital age offers.
+      </Text>
     </View>
   );
 }
@@ -13,8 +23,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f7f7f7',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  computerImage: {
+    width: 250,
+    height: 250,
+    resizeMode: 'cover',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#888',
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  description: {
+    fontSize: 16,
+    color: '#555',
+    textAlign: 'center',
+    paddingHorizontal: 20,
   },
 });
